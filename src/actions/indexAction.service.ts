7a89fbc6562e9c561c5428e195eae98e8d7fb86d
Kasun Hewage <kasunkhe@gmail.com>
2024-08-11 00:09:00 +0530

    log

--- src/actions/indexAction.service.ts
+++ src/actions/indexAction.service.ts
@@ -40,7 +40,7 @@ const indexServiceAction = async ({
       switch (response.status) {
         case 500:
           throw new Error("Service failed!", { cause: "SERVER_APP" });
-          logger.error("Service failed | " + response.status);
+
         case 400:
           throw new Error("Invalid data were passes!"!, {
             cause: "SERVER_APP",
@@ -53,6 +53,7 @@ const indexServiceAction = async ({
     const data = await response.json();
     return data;
   } catch (error) {
+    logger.error("Service failed | ");
     if (error instanceof Error) {
       if (error.cause === "SERVER_APP") {
         throw new Error(error.message, { cause: error.cause });
